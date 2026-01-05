634e0f429f4fcf314754d837d70645d3d7d31aaa
Uvissh <vishalbhateria@gmail.com>
2026-01-05 20:54:15 +0530

    review the request

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -59,6 +59,32 @@ const  requestRouter = express.Router();
    }
 
 })
+ requestRouter.post("/request/review/:status/:requestId",userAuth,async(req,res)=>{
+  try{
+    const loggedInUser = req.user;
+    //loggedInId is should of the  touserId because it is accepting or rejecting the request
+    //validate the status
+    const {status,requestId} = req.params;
+     const allowedStatus = ["accepted","rejected"];
+     if(!allowedStatus.includes(status)){
+      return res.status(400).json({message:"Status not allowed"});
 
+     }
+     const ConnectionRequest = await ConnectionRequestModel.findOne({
+      _id:requestId,
+      toUserId: loggedInUser._id,
+      status:"interested",
+     });
+     if(!ConnectionRequest){
+      return res.status(404).json({message:"Connection request not found"});
+     }
+     ConnectionRequest.status = status;//updating the status
+     const data = await ConnectionRequest.save();
+     res.json({message:"Connection request"+status,data});
+
+  }catch(err){
+    res.status(400).send("ERROR:"+ err.message);
+  }
+ })
 
 module.exports = requestRouter;
