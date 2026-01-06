66d30730f9252ee14bcd962c9ce85060a168ecb5
Uvissh <vishalbhateria@gmail.com>
2026-01-06 13:29:06 +0530

    comparing the ids toString()

--- src/routes/user.js
+++ src/routes/user.js
@@ -38,12 +38,23 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
         const connectionRequest = await ConnectionRequestModel.find({
             $or:[
                 {toUserId :loggedInUser._id,status:"accepted"},
-                {fromUserId:loggedInUser._id,status:"accepted"},
+                {fromUserId:loggedInUser._id,status:"accepted"},  //datatabase queries
             ],
 
-        }).populate("fromUserId",USER_SAFE_DATA);
+        }).populate("fromUserId",USER_SAFE_DATA)
+          .populate("toUserId",USER_SAFE_DATA);
 
-        const data = connectionRequest.map((row)=>row.fromUserId);
+          console.log(connectionRequest);
+          
+
+        const data = connectionRequest.map((row)=>
+        {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+
+        })
 
         res.json({data});
 
@@ -59,4 +70,4 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
 
 
 module.exports = userRouter;
-
+//you cannot compare the two mongo ids like this ===//wrong way
