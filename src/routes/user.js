d2562c599d3d84a2b3913dfda99138d66f59c95c
Uvissh <vishalbhateria@gmail.com>
2026-01-06 13:13:29 +0530

    connections and databasequeries refpopulate

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const userRouter = express.Router();
 const {userAuth} = require("../middleware/auth");
 const ConnectionRequestModel = require("../models/ConnectionRequest");
+const USER_SAFE_DATA =  "firstName lastName photoUrl age gender about skills"
 
 
 //get all the connection request
@@ -15,7 +16,8 @@ userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
             toUserId:loggedInUser._id,
             status:"interested",
 
-        }).populate("fromUserId",["firstName","lastName"])//herer we using the populate because of the referece ref = user
+        }).populate("fromUserId",USER_SAFE_DATA
+           )//herer we using the populate because of the referece ref = user
 
         res.json({
             message:"Data fetched successfully",
@@ -27,6 +29,30 @@ userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
         req.status(400).send("ERROR:"+err.message);
     }
 })
+
+
+userRouter.get("/user/connections",userAuth,async(req,res)=>{
+    try{
+
+        const loggedInUser = req.user;
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or:[
+                {toUserId :loggedInUser._id,status:"accepted"},
+                {fromUserId:loggedInUser._id,status:"accepted"},
+            ],
+
+        }).populate("fromUserId",USER_SAFE_DATA);
+
+        const data = connectionRequest.map((row)=>row.fromUserId);
+
+        res.json({data});
+
+    }catch(err){
+        res.status(400).send({message:err.message});
+    }
+})
+
+
 
 
 
